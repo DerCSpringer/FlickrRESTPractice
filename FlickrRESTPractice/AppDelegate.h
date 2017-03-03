4e55ad951169d89fbf67b2800a219145c1c5b6ae
Daniel Springer <dcs@Daniels-MacBook-Pro.local>
2017-03-03 11:30:09 -0800

    Initial Commit

---
+++ FlickrRESTPractice/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  FlickrRESTPractice
+//
+//  Created by Daniel Springer on 3/3/17.
+//  Copyright © 2017 Daniel Springer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ FlickrRESTPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlickrRESTPractice
+//
+//  Created by Daniel Springer on 3/3/17.
+//  Copyright © 2017 Daniel Springer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
