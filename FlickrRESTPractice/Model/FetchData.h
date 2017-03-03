e11c36743ab66de6554285cc67ba4428c4c4cc82
Daniel Springer <dcs@Daniels-MacBook-Pro.local>
2017-03-03 14:52:35 -0800

    FetchData classes

---
+++ FlickrRESTPractice/Model/FetchData.h
@@ -0,0 +1,28 @@
+//
+//  FetchData.h
+//  FlickrRESTPractice
+//
+//  Created by Daniel Springer on 3/3/17.
+//  Copyright © 2017 Daniel Springer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class FetchData;
+
+@protocol didFinishFetch <NSObject>
+
+@optional
+-(void)finished:(FetchData *)fetch with:(NSData *)data;
+-(void)finished:(FetchData *)fetch withJSON:(NSDictionary *)data;
+
+@end
+
+@interface FetchData : NSObject
+
+-(void)fetchDataAt:(NSURL *)url;
+-(void)fetchJSONDataAt:(NSURL *)url;
+
+@property (nonatomic, weak) id <didFinishFetch> delegate;
+
+@end
---
+++ FlickrRESTPractice/Model/FetchData.m
@@ -0,0 +1,21 @@
+//
+//  FetchData.m
+//  FlickrRESTPractice
+//
+//  Created by Daniel Springer on 3/3/17.
+//  Copyright © 2017 Daniel Springer. All rights reserved.
+//
+
+#import "FetchData.h"
+
+@implementation FetchData
+
+-(void)fetchDataAt:(NSURL *)url {
+    
+}
+
+-(void)fetchJSONDataAt:(NSURL *)url {
+    
+}
+
+@end
